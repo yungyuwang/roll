6b4f0d6e217c83cf8aa0ddf69ae837cb18438c1d
Wangyungyu <s1051700@gm.pu.edu.tw>
2018-03-23 00:13:28 +0800

    init

---
+++ Assets/camera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour {
+    public Transform player;
+    // Use this for initialization
+    Vector3 offset;
+	void Start () {
+		offset = transform.position - player.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = player.position + offset;
+	}
+}
---
+++ Assets/move.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+    Rigidbody rb;
+    public float speed;
+	// Use this for initialization
+	void Start () {
+
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        rb.AddForce(new Vector3(x, 0, z));
+
+    }
+}
